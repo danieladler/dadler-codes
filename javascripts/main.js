1dbdaeaba92bb74b6ae7e81ea036681d948a4a53
danieladler <weston.adler@gmail.com>
2017-02-17 08:51:57 -0800

    header social links & toggle display

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $('#links-social').click(function() {
-    $('#header-links-2').toggleClass('active');
+    // $('#header-links-2').toggleClass('active');
+    $('#header-links-2').slideToggle();
   });
 });
-  
